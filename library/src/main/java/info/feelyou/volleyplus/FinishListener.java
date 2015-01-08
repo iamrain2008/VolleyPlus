3c153d008deed95c09e35d22c1de87acad21a540
Remex Huang <yufei.huang@ele.me>
2015-01-08 15:24:19 +0800

    Add a finish callback interface

---
+++ library/src/main/java/info/feelyou/volleyplus/FinishListener.java
@@ -0,0 +1,13 @@
+package info.feelyou.volleyplus;
+
+/**
+ * Callback interface for http request finish
+ * <p/>
+ * Created by Yufei Huang
+ */
+public interface FinishListener {
+    /**
+     * Called when a http request is finish.
+     */
+    public void onFinish();
+}
